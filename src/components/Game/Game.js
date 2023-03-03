b5dd3835ff344cede82650da185b4025fe1197c9
David Garcia <davidgarciama@outlook.com>
2023-03-02 20:06:46 -0600

    almost done

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -6,6 +6,10 @@ import GuessInput from '../GuessInput';
 import PreviousGuess from '../PreviousGuess';
 import Guess from '../Guess';
 import guess from '../Guess';
+import Banner from '../Banner'; // TODO remove unusable code
+import HappyBanner from '../HappyBanner';
+import SadBanner from '../SadBanner';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -14,19 +18,35 @@ console.info({ answer });
 
 function Game() {
   const [guesses, setGuesses] = React.useState([]);
+  const [gameOver, setGameOver] = React.useState('running');
+  // console.log(guesses.length) //TODO remove comments
 
   const handlePreviousGuesses = (tentativeGuess) => {
     const newObject = {
       answer: tentativeGuess,
       id: crypto.randomUUID(),
     };
-    let newArray = [...guesses,newObject];
+    let newArray = [...guesses, newObject];
     setGuesses(newArray);
+    let game;
+    if (tentativeGuess === answer) {
+      game = 'won';
+      setGameOver(game);
+    } else if (newArray.length === NUM_OF_GUESSES_ALLOWED) {
+      game = 'lost';
+      setGameOver(game);
+    }
   };
+
   return (
     <>
       <PreviousGuess guesses={guesses} answer={answer}></PreviousGuess>
-      <GuessInput handlePreviousGuesses={handlePreviousGuesses} ></GuessInput>
+      <GuessInput
+        handlePreviousGuesses={handlePreviousGuesses}
+        gameOver={gameOver}
+      />
+      {gameOver === 'won' && <HappyBanner guessesLength={guesses.length} />}
+      {gameOver === 'lost' && <SadBanner answer={answer} />}
     </>
   );
 }
--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -2,25 +2,31 @@ import React from 'react';
 import { range } from '../../utils';
 import { checkGuess } from '../../game-helpers';
 
-function Guess({ value, answer }) {
+function Guess({ value, answer}) {
   //value -> {}
   //checkedValue -> [{},{}]
   let checkedValue;
-    if(value){
-      checkedValue = checkGuess(value.answer, answer);
-    }
+
+  if (value) {
+    checkedValue = checkGuess(value.answer, answer);
+  }
 
   return (
-    <p className='guess'>
+    <p className="guess">
       {range(5).map((cell) => (
         <span
-          className={checkedValue ? `cell ${checkedValue[cell].status}`:'cell'}
+          className={
+            checkedValue ? `cell ${checkedValue[cell].status}` : 'cell'
+          }
           key={value ? `${value.id}-${cell}` : cell}
         >
           {value ? value.answer[cell] : undefined}
         </span>
       ))}
+
     </p>
+
+
   );
 }
 
